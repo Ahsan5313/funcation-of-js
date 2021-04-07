dab3a9906f6f783a4fdaba482e87ddf502d7bf58
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-07 16:16:11 +0600

    add html and javascript for the project

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+
+
+function square(num1,num2){
+    sum=num1+num2;
+    document.write(" Sum =" + sum+"<br/>");
+}
+square((num1=parseFloat(prompt("Enter 1st number : "))),(num1=parseFloat(prompt("Enter 2nd number : " ))));
+
+
+function sub(num1,num2){
+    sub=num1-num2;
+    document.write(" Sum =" + sub+"<br/>");
+}
+sub((num1=parseFloat(prompt("Enter 1st number : "))),(num1=parseFloat(prompt("Enter 2nd number : " ))));
+
+
+function pere(num1,num2){
+    pere=num1*num2;
+    document.write(" Sum =" + pere+"<br/>");
+}
+pere((num1=parseFloat(prompt("Enter 1st number : "))),(num1=parseFloat(prompt("Enter 2nd number : " ))));
+
+
+function div(num1,num2){
+    div=num1/num2;
+    document.write(" Sum =" + div+"<br/>");
+}
+div((num1=parseFloat(prompt("Enter 1st number : "))),(num1=parseFloat(prompt("Enter 2nd number : " ))));
+
+
+function mul(num1,num2){
+    mul=num1%num2;
+    document.write(" Sum =" + mul+"<br/>");
+}
+mul((num1=parseFloat(prompt("Enter 1st number : "))),(num1=parseFloat(prompt("Enter 2nd number : " ))));
+
+
